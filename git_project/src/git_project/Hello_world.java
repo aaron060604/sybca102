6e6c92c64053f75ce436e6ae4748ec34d1c80e75
Velda <velda07dias@gmail.com>
2024-01-10 10:04:53 +0530

    first commit

---
+++ git_project/src/git_project/Hello_world.java
@@ -0,0 +1,11 @@
+package git_project;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
